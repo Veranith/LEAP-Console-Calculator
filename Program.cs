4cad331bc01d6397493decf411f07b7150248c90
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-08-23 20:35:01 -0600

    Add basic operation without validation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace LEAP_Console_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter number: ");
+            var input1 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter number: ");
+            var input2 = Convert.ToDecimal(Console.ReadLine());
+            //TODO input validation
+
+            Console.Write("Enter operator ( X / + - )  :");
+            string operator1 = Console.ReadLine();
+            // TODO input validation
+            
+            decimal result = 0;
+            switch (operator1.ToLower())
+            {
+                case "x":
+                case "*":
+                    result = input1 * input2;
+                    break;
+                case "/":
+                case "\\":
+                    result = input1 / input2;
+                    break;
+                case "+":
+                    result = input1 + input2;
+                    break;
+                case "-":
+                    result = input1 - input2;
+                    break;
+            }
+
+            Console.WriteLine($" {input1} {operator1} {input2} = {result}");
+            
         }
     }
 }
