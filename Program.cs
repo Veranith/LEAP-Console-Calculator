7fbd766aebe1636ce5272bbbd44409f9c982170b
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-08-23 19:08:05 -0600

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LEAP_Console_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
