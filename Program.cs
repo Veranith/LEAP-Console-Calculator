96a9449009035d85f08fe1ad9e1907ec885cc4ed
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-08-23 22:17:29 -0600

    Add number input validation with class.

--- Program.cs
+++ Program.cs
@@ -6,36 +6,44 @@ namespace LEAP_Console_Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter number: ");
-            var input1 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Enter number: ");
-            var input2 = Convert.ToDecimal(Console.ReadLine());
-            //TODO input validation
-
-            Console.Write("Enter operator ( X / + - )  :");
-            string operator1 = Console.ReadLine();
-            // TODO input validation
-            
-            decimal result = 0;
-            switch (operator1.ToLower())
+            try
             {
-                case "x":
-                case "*":
-                    result = input1 * input2;
-                    break;
-                case "/":
-                case "\\":
-                    result = input1 / input2;
-                    break;
-                case "+":
-                    result = input1 + input2;
-                    break;
-                case "-":
-                    result = input1 - input2;
-                    break;
-            }
+                var inputs = new utilities();
+                
+                decimal input1 = inputs.getNumber("Enter number 1 (numbers only): ");
+                decimal input2 = inputs.getNumber("Enter number 2 (numbers only): ");
+                
+                Console.Write("Enter operator ( X / + - )  :");
+                string operator1 = Console.ReadLine();
+                // TODO input validation
+            
+                decimal result = 0;
+                switch (operator1.ToLower())
+                {
+                    case "x":
+                    case "*":
+                        result = input1 * input2;
+                        break;
+                    case "/":
+                    case "\\":
+                        result = input1 / input2;
+                        break;
+                    case "+":
+                        result = input1 + input2;
+                        break;
+                    case "-":
+                        result = input1 - input2;
+                        break;
+                }
 
-            Console.WriteLine($" {input1} {operator1} {input2} = {result}");
+                Console.WriteLine($" {input1} {operator1} {input2} = {result}");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Unable to divide by 0.");
+                //throw;
+            }
             
         }
     }
---
+++ utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LEAP_Console_Calculator
+{
+    class utilities
+    {
+        public decimal getNumber(string message)
+        {
+
+            string userInput;
+            decimal result;
+            do
+            {
+                userInput = getInput(message);
+            } 
+            while (!Decimal.TryParse(userInput, out result));
+            
+            return result;
+
+        }
+
+        private string getInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
